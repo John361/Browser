00f4f7509c66d3943b9aca27e9764cb8c53083de
Hoareau John <hxjwork1@gmail.com>
2018-06-28 21:08:44 +0400

    Solve issue [#1](https://gitlab.com/John361/Browser/issues/1)

--- layoutmainwindow.cpp
+++ layoutmainwindow.cpp
@@ -4,6 +4,7 @@ LayoutMainWindow::LayoutMainWindow(QWidget *parent) : QVBoxLayout(parent)
 {
     m_tab = new Tab;
     addWidget(m_tab);
+    setContentsMargins(0, 0, 0, 0);
 }
 
 Tab *LayoutMainWindow::tab() const
